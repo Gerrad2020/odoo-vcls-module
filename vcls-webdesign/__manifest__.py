a18f001ef778e708284ad1097cf7aa162f56673a
blouk <nicolas@blouk.com>
2019-03-08 09:55:28 +0100

    [ADD] Add 2 cols form builder snippet

---
+++ vcls-webdesign/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'VCLS webdesign',
+    'summary': "VCLS webdesign",
+    'version': '0.1',
+    'author': "Odoo",
+    'category': 'Website',
+    'depends': ['website','website_form_editor'],
+    'data': [
+
+        #frontend
+        'views/snippets.xml',
+
+    ],
+    'auto_install': True,
+    'installable': True,
+}
