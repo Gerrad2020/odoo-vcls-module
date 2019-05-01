848949f8097e0fdf62ed740e7d411f145114a47a
mathieugaillard <43413290+mathieugaillard@users.noreply.github.com>
2019-05-01 16:58:19 +0200

    leave duration back to origin

--- vcls-hr/models/leave.py
+++ vcls-hr/models/leave.py
@@ -83,7 +83,22 @@ class Leave(models.Model):
     #####################
     # Overriden Methods #
     #####################
+
     #We simplify this method to force the number of days to be based on days and not hours.
+    def _get_number_of_days(self, date_from, date_to, employee_id):
+        """ Returns a float equals to the timedelta between two dates given as string."""
+        if employee_id:
+            employee = self.env['hr.employee'].browse(employee_id)
+            return employee.get_work_days_data(date_from, date_to)['days']
+
+        today_hours = self.env.user.company_id.resource_calendar_id.get_work_hours_count(
+            datetime.combine(date_from.date(), time.min),
+            datetime.combine(date_from.date(), time.max),
+            False)
+
+        return self.env.user.company_id.resource_calendar_id.get_work_hours_count(date_from, date_to) / (today_hours or HOURS_PER_DAY)
+
+    """#We simplify this method to force the number of days to be based on days and not hours.
     def _get_number_of_days(self, date_from, date_to, employee_id):
         # Returns a float equals to the timedelta between two dates given as string.
         if employee_id:
@@ -103,17 +118,7 @@ class Leave(models.Model):
             datetime.combine(date_from.date(), time.max),
             False)
 
-        return self.env.user.company_id.resource_calendar_id.get_work_hours_count(date_from, date_to) / (today_hours or HOURS_PER_DAY)
-        
-        """
-        delta = date_to.date()-date_from.date()
-        if self.request_unit_half:
-            in_days = 0.5 #half a day is half a day
-        else:
-            in_days = delta.days + 1 #we add one to cover the current day (i.e. if start = end date)
-        
-        return in_days
-        """
+        return self.env.user.company_id.resource_calendar_id.get_work_hours_count(date_from, date_to) / (today_hours or HOURS_PER_DAY)"""
         
     def activity_update(self):
         default_deadline = datetime.today() + relativedelta(weeks=1)
